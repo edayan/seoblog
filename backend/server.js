73a9cb45d90d23f5b878644d3daa198dbdb09cf5
Saju Paul <saju.christel@blu-pa.com>
2020-05-13 22:35:57 +0530

    running servers

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,23 @@ const cors = require('cors')
 require('dotenv').config()
 
 
+// app
+const app = express();
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+// cors
+app.use(cors());
+
+// routes
+app.get('/api', (req, res) => {
+    res.json({ time: Date().toString() });
+});
+
+// port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
