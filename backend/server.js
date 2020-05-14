332d8450a850ff48e37a8038b43ca8a87020e521
Saju Paul <saju.christel@blu-pa.com>
2020-05-14 21:29:39 +0530

    addign mongo db config and connecting to it

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const cookieParser  = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 
 // app
 const app = express();
 
+// db
+mongoose
+    .connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+    .then(() => console.log('DB connected'));
+
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 // cors
-app.use(cors());
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
 // routes
 app.get('/api', (req, res) => {
