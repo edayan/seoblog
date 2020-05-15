bbc1dc0acf8e7c7bd7a1bdd681f4e1b60befbcfc
Saju Paul <saju.christel@blu-pa.com>
2020-05-15 22:41:54 +0530

    user sign in- backend

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,8 @@
 const User = require('../models/user');
 const shortId = require('shortid');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt')
+
 
 exports.signup = (req, res) => {
     User.findOne({ email: req.body.email }).exec((err, user) => {
@@ -29,5 +32,41 @@ exports.signup = (req, res) => {
             });
         });
     });
+
+};
+
+exports.signin = (req, res) => {
+
+    const { email, password } = req.body;
+    User.findOne({ email }).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User does not exists'
+            });
+        }
+
+
+        //authenticate
+        if (!user.authenticate(password)) {
+            return res.status(400).json({
+                error: 'email or password does not match'
+            });
+        }
+
+        // generate a token and send to client
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        res.cookie('token', token, { expiresIn: '1d' });
+        const { _id, username, name, email, role } = user;
+        return res.json({
+            token,
+            user: { _id, username, name, email, role }
+        });
+
+    });
+
+
+
+
 
 };
