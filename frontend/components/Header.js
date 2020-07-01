3378f7d4673f4eeafc30677701e625f99c227017
Saju Paul <saju.christel@blu-pa.com>
2020-07-01 18:25:13 +0530

    Adding environment variables

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import {APP_NAME} from '../config';
+
 import {
   Collapse,
   Navbar,
@@ -21,7 +23,7 @@ const Header = (props) => {
 
   return (
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarBrand href="/">{APP_NAME}</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
